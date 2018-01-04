bc5434b83c4863926bfca835dccb2eeba80c1ae7
bobo liu <admin@boboweb.net>
2018-01-04 16:03:41 +0800

    Give up using travisCI to auto release

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 mkdir -p dist/utils
-go build -o dist/sShare github.com/popu125/sShare 
-go build -o dist/utils/simplepage-gen github.com/popu125/sShare/utils/simplepage-gen 
-cp config.example.json dist/config.json
-cp utils/simplepage-gen/index.tpl dist/utils/index.tpl
-upx -9 dist/*
-tar czf build-$GOARCH.tar.gz dist/*
+rm -rf dist/*
+for arch in "386" "amd64"; do
+	echo "Start building $arch."
+	export GOARCH=$arch
+	export GOOS=linux
+	go build -o dist/sShare -ldflags="-s -w" github.com/popu125/sShare 
+	go build -o dist/utils/simplepage-gen -ldflags="-s -w" github.com/popu125/sShare/utils/simplepage-gen 
+	cp config.example.json dist/config.json
+	cp utils/simplepage-gen/index.tpl dist/utils/index.tpl
+	upx dist/sShare dist/utils/simplepage-gen
+	echo "Packing $arch."
+	tar -C dist/ -czf build-$GOARCH.tar.gz $(ls dist)
+done
+echo "Build Done."
