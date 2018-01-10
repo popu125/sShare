3dc96cf55704b207bb1699424c4cf40241f472b1
bobo liu <admin@boboweb.net>
2018-01-10 11:55:11 +0800

    Disable proc check api.

--- web/route.go
+++ web/route.go
@@ -10,7 +10,7 @@ func GetRouter(api *ApiServe) *mux.Router {
 	s := r.PathPrefix("/api").Subrouter()
 	s.HandleFunc("/count", api.serveCount).Methods("POST")
 	s.HandleFunc("/new", api.newProc).Methods("POST")
-	s.HandleFunc("/check/{procid}", api.procCheck)
+	//s.HandleFunc("/check/{procid}", api.procCheck)
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
 
 	return r
